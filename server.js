081830b2fbef7bfc1aca3b969127f60f5253f285
Eric (Mansa) Marah <devopsmansa@gmail.com>
2024-02-15 13:00:02 -0600

    Uploaded express.js files

---
+++ server.js
@@ -0,0 +1,18 @@
+//I required express library
+const express = require(`express`)
+
+// i run express and store return value in the app variable
+const app = express()
+//saved port number as a variable for dynamic purposes
+const PORT = 3000
+
+//Middleware
+
+
+//ROUTES
+
+//we must listen to the server on specified PORT
+//LISTENING SHOULD ALWAYS BE THE LAST THING ON THE FILE
+app.listen(PORT), () =>{
+    console.log(`Server listening on port: ${PORT}`)
+}
